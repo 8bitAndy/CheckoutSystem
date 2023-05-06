5f9a777a9fec4749a12d0659cf9858670a746469
Liam <86295301+8bitAndy@users.noreply.github.com>
2023-05-06 22:55:38 +0800

    Added JsonReader class
    Added basic menu in main and JsonReader class

---
+++ CheckoutSystem/CheckoutSystem/JsonReader.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutSystem
+{
+    internal class JsonReader
+    {
+        public void readJSON()
+        {
+            string filename = "inventory.json";
+            string path = Path.Combine(Environment.CurrentDirectory, @"", filename);
+
+
+            // Get the JSON file for inventory and assign it to a variable
+            using (StreamReader r = new StreamReader(path))
+            {
+                // Get all data from JSON file and assign to string
+                string json = r.ReadToEnd();
+
+                // Prevents exception with empty JSON file
+                if (!string.IsNullOrEmpty(json))
+                {
+                    // Ignore error, some issue with Newtonsoft package
+                    List<InventoryItem> InventoryList = JsonConvert.DeserializeObject<List<InventoryItem>>(json);
+
+                    // Check file contains items in it, then loop through and print values
+                    if (InventoryList.Count > 0)
+                    {
+                        foreach (var output in InventoryList)
+                        {
+                            Console.WriteLine(output.id);
+                            Console.WriteLine(output.name);
+
+                        }
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("File is empty");
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
--- CheckoutSystem/CheckoutSystem/Program.cs
+++ CheckoutSystem/CheckoutSystem/Program.cs
@@ -1,9 +1,54 @@
-﻿namespace CheckoutSystem
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace CheckoutSystem
 {
-    class CheckoutSystem { 
+    class CheckoutSystem {
+        public static int menuChoice = 0;
         static void Main(string[] args)
         {
-            // TODO add code
+            // Main event loop for program menu
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter a menu option:\n");
+                    Console.WriteLine("1. Review Inventory");
+                    Console.WriteLine("2. Add item to inventory");
+                    Console.WriteLine("3. Take item from inventory");
+                    Console.WriteLine("4. Calculate total value of inventory");
+                    Console.Write("Your choice: ");
+                    menuChoice = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine(menuChoice + " option is");
+                }
+                catch(System.FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                
+
+            }
+
+            // Creating new instance of the json reader
+            JsonReader data = new JsonReader();
+
+            // Read data from JSON file
+            data.readJSON();
         }
+    }
+
+    //
+    class InventoryItem
+    {
+        [JsonProperty("id")]
+        public int id { get; set; }
+        [JsonProperty("name")]
+        public string ?name { get; set; }
+        [JsonProperty("description")]
+        public string ?description { get; set; }
+        [JsonProperty("cost")]
+        public float cost { get; set; }
     }
 }
